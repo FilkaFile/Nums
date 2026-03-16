192d5ba142eba88340f7db7be15c095e2a0b8c99
Filka <space.herringtoo@gmail.com>
2026-03-16 12:29:38 +0300

    API

--- FAPI.py
+++ FAPI.py
@@ -1,10 +1,50 @@
+import uvicorn
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse, Response, PlainTextResponse, FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
+My_Number = 1*3442
 
-@app.get('/')
+
+origins = ["*"]
+app.add_middleware(CORSMiddleware, allow_origins=origins,
+                   allow_methods=['*'], allow_headers=['*'])
+
+
+my_us = {'1': 'Ivan', "2": 'Pavel', '3': 'Stepa'}
+
+
+@app.get('/users/{id}/{age}')  # Шаблон
+def get_users(id, age):
+    return str(my_us.get(id)) + " age " + str(age)
+
+
+@app.get('/num')
+def mynum():
+    return My_Number
+
+
+@app.get('/foo')
+def root_file():
+    return FileResponse("public/ooo.docx", filename='ooo.docx', media_type="application/octet-stream")
+
+
+@app.get('/', response_class=PlainTextResponse)
 def read_root():
+    data = "ОПАНА"
+    return data
+
     html_content = '<h2>Hello world</h2>'
-    return HTMLResponse(content=html_content)
+    return {"message": html_content}
+
+
+@app.get('/about')
+def about():
+    return {"message": "1 на сайте"}
+
+
+if __name__ == "__main__":
+    uvicorn.run('FAPI:app', host='0.0.0.0', port=8000, reload=True)
