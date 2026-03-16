808acf2767616b3c82da198ed4cf2cef8ffb9f20
Filka <space.herringtoo@gmail.com>
2026-03-16 17:06:55 +0300

    ЩЩЩЩ

--- FAPI.py
+++ FAPI.py
@@ -1,9 +1,12 @@
+import math
+from dataclasses import dataclass, field, InitVar, make_dataclass
+from dataclasses import asdict
 import uvicorn
 from fastapi import FastAPI, Path, Query
 from fastapi.responses import HTMLResponse, JSONResponse, Response, PlainTextResponse, FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 
-
+import json
 app = FastAPI()
 
 My_Number = 1*3442
@@ -15,6 +18,113 @@ app.add_middleware(CORSMiddleware, allow_origins=origins,
 
 
 my_us = {'1': 'Ivan', "2": 'Pavel', '3': 'Stepa'}
+
+# 1
+
+
+@app.get("/calc")
+def get_calc(a: float, b: float, oper):
+    if oper == 'add':
+        res = a+b
+
+    elif oper == 'subtract':
+        res = a-b
+
+    elif oper == 'multiply':
+        res = a*b
+
+    elif oper == 'divide':
+        res = a/b
+
+    else:
+        res = 'null, одна ошибка и вы ошиблись'
+
+    return {"результате": res}
+
+
+# 2
+
+bookshelf = list()
+filtred_bookshelf = list()
+
+
+@dataclass
+class Book:
+    id: int
+    title: str
+    author: str
+    year: int
+    genre: str
+
+
+bookshelf = [
+    Book(id=1, title='Война и пирог', author='Сергей', year=1990, genre='хоррор'),
+    Book(id=2, title='Мэрри момент', author='Олег', year=2000, genre='хоррор'),
+    Book(id=3, title='Потапыч', author='Олег', year=1890, genre='комедия'),
+    Book(id=4, title='Мёртвые туши', author='Сергей', year=2000, genre='комедия'),
+    Book(id=5, title='Шерлок Шомс', author='Сергей', year=2020, genre='комедия'),
+    Book(id=6, title='Чипсик', author='Олег', year=2000, genre='комедия')
+]
+
+
+@app.get("/books")
+def get_book(author: str = Query(default=''), genre: str = Query(default=''), min_year: int = Query(default=0), max_year: int = Query(default=0)):
+
+    filtered_books = [book for book in bookshelf if (
+        (not author or book.author == author) and
+        (not genre or book.genre == genre) and
+        (not min_year or book.year >= min_year) and
+        (not max_year or book.year <= max_year)
+    )]
+
+    return save_to_json(filtered_books, 'filtered_books.json')
+
+
+def save_to_json(filtered_books, filename):
+    data = str(filtered_books)
+
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
+
+        return FileResponse("filtered_books.json", filename='filtered_books.json', media_type="application/octet-stream")
+
+# 3
+
+
+@dataclass
+class Movie:
+    id: int
+    title: str
+    views_history: list[int]
+
+
+movies = [Movie(1, 'Абоба', [12, 123, 12, 54, 34, -65]),
+          Movie(2, 'Фнаф', [110, -22, -32]),
+          Movie(3, 'КрикАААА', [12, 123, 12, 54, 34, 65])
+          ]
+
+
+@app.get("/movies")
+def get_movie(status: str | None = None, min_avg_views: int = Query(default=0)):
+    filtr_movies = []
+    for opa in movies:
+        last_day = opa.views_history[-1]
+        avg = sum(opa.views_history) / len(opa.views_history)
+        rost = (last_day - avg) / avg * 100
+
+        if rost > 20:
+            trend_status = "Viral"
+
+        elif -10 < rost <= 20:
+            trend_status = "Stable"
+
+        else:
+            trend_status = "Fading"
+
+        if trend_status == status and avg >= min_avg_views:
+            filtr_movies.append(opa)
+
+    return str(filtr_movies)
 
 
 @app.get("/cats")
