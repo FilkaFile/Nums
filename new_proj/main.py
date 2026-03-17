1a542e7450cf4d08982ac6b9d1307baa92251628
Filka <space.herringtoo@gmail.com>
2026-03-17 12:00:54 +0300

    ssssss

--- new_proj/main.py
+++ new_proj/main.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 import uvicorn
-from fastapi import FastAPI, Body, Path
+from fastapi import FastAPI, Body, Path, status
 from fastapi.responses import (
     Response, HTMLResponse, JSONResponse, PlainTextResponse, FileResponse, RedirectResponse)
 from pydantic import BaseModel, Field
@@ -27,6 +27,60 @@ def find_person(id):
         if person.id == id:
             return person
     return None
+
+
+@app.get('/api/users')
+def get_people():
+    return people
+
+
+@app.get('/')
+def root():
+    return FileResponse("public/index.html")
+
+
+@app.get("/api/users/{id}")
+def get_perdon(id):
+    person = find_person(id)
+    if person == None:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"message": "Пользователь хз где"}
+        )
+    return person
+
+
+@app.delete("/api/users/{id}")
+def delete_perdon(id):
+    person = find_person(id)
+    if person == None:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"message": "Пользователь хз где"}
+        )
+    people.remove(person)
+    return person
+
+
+@app.post("/api/users")
+def create_person(data=Body()):
+    person = Person(data["name"], data["age"])
+    people.append(person)
+    return person
+
+
+@app.put("/api/users/{id}")
+def edit_peron(data=Body()):
+    person = find_person(data["id"])
+    if person == None:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"message": "Пользователь хз где"}
+        )
+    person.age = data["age"]
+    person.name = data["name"]
+
+    return person
 
 
 if __name__ == "__main__":
